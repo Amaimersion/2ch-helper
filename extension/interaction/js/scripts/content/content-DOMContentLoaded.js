937a2bdbab9673884ab226c07a894e65941b4d68
Amaimersion <amatsukine@gmail.com>
2018-02-13 21:25:11 +0300

    Add a documentation for content-DOMContentLoaded.js

--- extension/interaction/js/scripts/content/content-DOMContentLoaded.js
+++ extension/interaction/js/scripts/content/content-DOMContentLoaded.js
@@ -1,18 +1,44 @@
+/** 
+ * The module that run a main function when 
+ * a page has the status 'DOMContentLoaded'.
+ * 
+ * @module ContentDOMContentLoaded
+ */
 function ContentDOMContentLoaded() {}
 
 
+/**
+ * Runs when a page has the status 'DOMContentLoaded'.
+ * 
+ * @memberof ContentDOMContentLoaded
+ * @static
+ */
 ContentDOMContentLoaded.main = function() {
     this.bindCheckboxes();
 }
 
 
-ContentDOMContentLoaded.bindCheckboxes = function() {
+/**
+ * Binds events to checkboxes on a page.
+ * 
+ * @memberof ContentDOMContentLoaded
+ * @static
+ * @param {String} [checkboxesSelector=input[type="checkbox"]] 
+ * A query selector for finding checkboxes. 
+ * Defaults to 'input[type="checkbox"]'.
+ */
+ContentDOMContentLoaded.bindCheckboxes = function(checkboxesSelector) {
+    checkboxesSelector = checkboxesSelector || 'input[type="checkbox"]';
     const thread = ContentAPI.getThread();    
-    const checkboxes = thread.querySelectorAll('input[type="checkbox"]');
+    const checkboxes = thread.querySelectorAll(checkboxesSelector);
+
     ContentPageElements.addEventsToCheckboxes(checkboxes);
 }
 
 
+/** 
+ * Adds event listener to the page. 
+ */
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', main);
 } else {
