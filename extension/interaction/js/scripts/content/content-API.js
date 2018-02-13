2e357309cadd85a06d86459933a3b7a44394ce6c
Amaimersion <amatsukine@gmail.com>
2018-02-13 21:11:59 +0300

    Add a documentation for content-API.js

--- extension/interaction/js/scripts/content/content-API.js
+++ extension/interaction/js/scripts/content/content-API.js
@@ -1,6 +1,17 @@
+/** 
+ * The common module for usage in others content modules.
+ * 
+ * @module ContentAPI
+ */
 function ContentAPI() {}
 
 
+/**
+ * Information about other content modules.
+ * 
+ * @memberof ContentAPI
+ * @static
+ */
 ContentAPI.anotherAPI = {
     'screenshot': {
         injected: false,
@@ -40,6 +51,18 @@ ContentAPI.anotherAPI = {
 };
 
 
+/**
+ * Injects another module and executes the method of that module.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @async
+ * @param {String} name A name of another content module.
+ * @param {String} method A method of another content module.
+ * @returns {Promise<undefined>} 
+ * A promise for the execute that will resolve when injects and executes are successfully completed.
+ * Resolve will contain undefined if success, otherwise reject will contain an error.
+ */
 ContentAPI.executeAnotherAPI = function(name, method) {
     return new Promise((resolve, reject) => {
         const injectPromise = this.injectAnotherAPI(name);
@@ -62,6 +85,17 @@ ContentAPI.executeAnotherAPI = function(name, method) {
 }
 
 
+/**
+ * Injects another module into the page.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @async
+ * @param {String} name A name of another content module.
+ * @returns {Promise<undefined>}
+ * A promise for the injection that will resolve when the module will injects.
+ * Resolve will contain undefined if success, otherwise reject will contain an error. 
+ */
 ContentAPI.injectAnotherAPI = function(name) {
     return new Promise((resolve, reject) => {
         const status = this.anotherAPI[name].injected;
@@ -94,6 +128,18 @@ ContentAPI.injectAnotherAPI = function(name) {
 }
 
 
+/**
+ * Executes a method of another module.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @async
+ * @param {String} name A name of another content module.
+ * @param {String} method A method of another content module.
+ * @returns {Promise<undefined>} 
+ * A promise for the execute that will resolve when the method will starts.
+ * Resolve will contain undefined if success, otherwise reject will contain an error.
+ */
 ContentAPI.executeAnotherAPIMethod = function(name, method) {
     return new Promise((resolve, reject) => {
         const anotherAPIMethod = this.anotherAPI[name].methods[method];
@@ -115,6 +161,14 @@ ContentAPI.executeAnotherAPIMethod = function(name, method) {
 }
 
 
+/**
+ * Gets a thread. Search occurs by id of a thread.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @param {String} [id=posts-form] An id of thread. Defaults to 'posts-form'.
+ * @returns {HTMLElement} A thread if it was finded, otherwise null.
+ */
 ContentAPI.getThread = function(id) {
     return document.getElementById(id || 'posts-form');
 }
@@ -123,15 +177,18 @@ ContentAPI.getThread = function(id) {
 /**
 * Gets a parent of the HTML element.
 * 
+* @memberof ContentAPI
+* @static
+
 * @param {HTMLElement} element 
 * A beginning element.
 * 
-* @param {function(HTMLElement) => Boolean} condition
-* A condition to complete.
+* @param {function(HTMLElement) => Boolean} [condition]
+* A condition to complete. Defaults does not affect the search.
 * 
 * @returns {HTMLElement}
-* If condition was not declared, then document will be return.
-* Otherwise element which satisfies condition will be return.
+* If the condition was not declared, then document will be return.
+* Otherwise an element which satisfies the condition will be return.
 */
 ContentAPI.getParent = function(element, condition) {
    condition = condition || function(element) {return false;};
@@ -146,6 +203,14 @@ ContentAPI.getParent = function(element, condition) {
 }
 
 
+/**
+ * Sends a message to the background scripts.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @param {Object} message A message for sending.
+ * @param {function(Object)} [callback] A callback that handle the response.
+ */
 ContentAPI.sendMessageToBackground = function(message, callback) {
    callback = callback || function() {};
 
