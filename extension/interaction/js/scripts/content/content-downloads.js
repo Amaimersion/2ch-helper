8cc55a5f72411bb02c526c2733d7023625050f71
Amaimersion <amatsukine@gmail.com>
2018-02-14 09:43:18 +0300

    Add a documentation for content-downloads.js

--- extension/interaction/js/scripts/content/content-downloads.js
+++ extension/interaction/js/scripts/content/content-downloads.js
@@ -1,28 +1,62 @@
+/** 
+ * The module that handle download requests.
+ * 
+ * @module ContentDownloads
+ */
 function ContentDownloads() {}
 
 
+/**
+ * Downloads images.
+ * 
+ * @memberof ContentDownloads
+ * @static
+ * @async
+ * @param {function(Object)} [callback] 
+ * A callback that executes after download and handles the response.
+ */
 ContentDownloads.downloadImages = function(callback) {
     callback = callback || function() {};
 
     this.processDownload(
         'img.preview:not(.webm-file)',
         'downloadImages',
+        null,
         callback
     );
 }
 
 
+/**
+ * Downloads video.
+ * 
+ * @memberof ContentDownloads
+ * @static
+ * @async
+ * @param {function(Object)} [callback] 
+ * A callback that executes after download and handles the response.
+ */
 ContentDownloads.downloadVideo = function(callback) {
     callback = callback || function() {};
     
     this.processDownload(
         'img.webm-file',
         'downloadVideo',
+        null,
         callback
     );
 }
 
 
+/**
+ * Downloads media content.
+ * 
+ * @memberof ContentDownloads
+ * @static
+ * @async
+ * @param {function(Object)} [callback] 
+ * A callback that executes after download and handles the response.
+ */
 ContentDownloads.downloadMedia = function(callback) {
     callback = callback || function() {};
  
@@ -32,30 +66,56 @@ ContentDownloads.downloadMedia = function(callback) {
 }
 
 
+/**
+ * Downloads thread.
+ * 
+ * @memberof ContentDownloads
+ * @static
+ * @async
+ * @param {function(Object)} [callback] 
+ * A callback that executes after download and handles the response.
+ */
 ContentDownloads.downloadThread = function(callback) {
     callback = callback || function() {};
     const message = {type: 'command', command: 'downloadThread'};
 
-    ContentAPI.sendMessageToBackground(message, () => {
-        callback();
+    ContentAPI.sendMessageToBackground(message, (response) => {
+        callback(response);
     });
 }
 
 
-// я не указываю параметр condition.
-// если в странице изменится структура, 
-// то этот параметер может понадобиться.
-ContentDownloads.processDownload = function(query, command, callback) {
+/**
+ * Downloads an elements.
+ * 
+ * @memberof ContentDownloads
+ * @static
+ * @async 
+ * 
+ * @param {String} selector 
+ * A query selector for getting preview elements.
+ * 
+ * @param {String} command 
+ * A command that executes download process in background script.
+ * Can be 'downloadThread', or 'downloadImages', or 'downloadVideo'.
+ * 
+ * @param {function(HTMLElement) => Boolean} [condition] 
+ * A condition for getting element href. 
+ * Defaults to (return element.tagName === 'A').
+ * 
+ * @param {function(Object)} [callback] 
+ * A callback that executes after download and handles the response.
+ */
+ContentDownloads.processDownload = function(selector, command, condition, callback) {
     const thread = ContentAPI.getThread();
-    let previewElements = thread.querySelectorAll(query);
-    const fullElements = [];
+    let previewElements = thread.querySelectorAll(selector);
+
+    const fullElementsHrefs = [];
+    condition = condition || function(element) {return element.tagName === 'A'};
 
     for (let element of previewElements) {
-        let fullElement = ContentAPI.getParent(element, (elem) => {
-            return (elem.tagName === 'A');
-        });
-
-        fullElements.push(fullElement.href);
+        let fullElement = ContentAPI.getParent(element, condition);
+        fullElementsHrefs.push(fullElement.href);
     }
 
     previewElements = [];
@@ -63,7 +123,7 @@ ContentDownloads.processDownload = function(query, command, callback) {
     const message = {
         type: 'command',
         command: command,
-        data: fullElements
+        data: fullElementsHrefs
     };
 
     ContentAPI.sendMessageToBackground(message, (response) => {
