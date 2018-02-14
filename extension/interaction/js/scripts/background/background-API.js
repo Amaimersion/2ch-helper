4fa7bdd1cfca48d0d5184fef5d95b8ece240a407
Amaimersion <amatsukine@gmail.com>
2018-02-14 19:01:26 +0300

    Add a documentation for background-API.js

--- extension/interaction/js/scripts/background/background-API.js
+++ extension/interaction/js/scripts/background/background-API.js
@@ -1,11 +1,38 @@
+/** 
+ * The common module for usage in others background modules.
+ * 
+ * @module BackgroundAPI
+ */
 function BackgroundAPI() {}
 
 
+/**
+ * Clears a cashe of the background page.
+ * It's just reload it, but it's works.
+ * 
+ * @memberof ContentAPI
+ * @static
+ */
 BackgroundAPI.clearCashe = function() {
     window.location.reload(true);
 }
 
 
+/**
+ * Injects a script into the page.
+ * 
+ * @memberof ContentAPI
+ * @static
+ * @async
+ * 
+ * @param {Object} options 
+ * An options for execute. 
+ * See https://developer.chrome.com/extensions/tabs#method-executeScript
+ * 
+ * @returns {Promise<undefined>} 
+ * A promise for the inject that will resolve when injects are successfully completed.
+ * Resolve will contain undefined if success, otherwise reject will contain an error.
+ */
 BackgroundAPI.injectScript = function(options) {
     return new Promise((resolve, reject) => {
         chrome.tabs.executeScript(options, (result) => {
