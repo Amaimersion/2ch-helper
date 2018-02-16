27842b01f3c2276921bacf6995dfd333e3762b88
Amaimersion <amatsukine@gmail.com>
2018-02-16 23:28:35 +0300

    Add structure and style for download and screenshot settings

--- extension/interface/js/scripts/settings-downloads.js
+++ extension/interface/js/scripts/settings-downloads.js
@@ -1,5 +1,13 @@
 function f() {
-    var slider = new Slider('#ex6');
-  }
+
+
+  var slider2 = new Slider('#downloadDelay', {
+    tooltip: 'hide'
+  });
+
+  slider2.on("slide", function(value) {
+    document.getElementById('downloadDelayValue').textContent = value + 'мс';
+  });
+}
 
 document.addEventListener("DOMContentLoaded", f)
--- extension/interface/js/scripts/settings-screenshots.js
+++ extension/interface/js/scripts/settings-screenshots.js
@@ -1,6 +1,19 @@
 function f() {
-    var slider = new Slider('#ex6');
-    var slider2 = new Slider('#ex7');
+    var slider = new Slider('#screenshotQuality', {
+      tooltip: 'hide'
+    });
+
+    slider.on("slide", function(value) {
+      document.getElementById('screenshotQualityValue').textContent = value + '%';
+    });
+
+    var slider2 = new Slider('#screenshotDelay', {
+      tooltip: 'hide'
+    });
+
+    slider2.on("slide", function(value) {
+      document.getElementById('screenshotDelayValue').textContent = value + 'мс';
+    });
   }
 
   document.addEventListener("DOMContentLoaded", f)
