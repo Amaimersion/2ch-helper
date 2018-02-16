1bfe0e7e5a305a7580608bf2a8fd7fbd1e72c475
Amaimersion <amatsukine@gmail.com>
2018-02-17 00:07:58 +0300

    Start create interactivity in settings

---
+++ extension/interface/js/scripts/settings.js
@@ -0,0 +1,48 @@
+function Settings() {}
+
+Settings.iframeData = [
+    {
+        navbarId: 'settings-screenshot-navbar',
+        elementId: 'settings-screenshot',
+        height: '430px'
+    },
+    {
+        navbarId: 'settings-download-navbar',
+        elementId: 'settings-download',
+        height: '220px'
+    }
+];
+
+
+Settings.main = function() {
+    Settings.bindElements();
+}
+
+
+Settings.bindElements = function() {
+    for (let element of Settings.iframeData) {
+        document.getElementById(element.navbarId).onclick = function() {
+            Settings.hideAllIframes();
+
+            const iframe = document.getElementById(element.elementId);
+            
+            iframe.style.display = 'block';
+            iframe.style.height = element.height;
+        }
+    }
+}
+
+Settings.hideAllIframes = function() {
+    const iframes = document.querySelectorAll("iframe");
+
+    for (let iframe of iframes) {
+        iframe.style.display = 'none';
+    }
+}
+
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', Settings.main);
+} else {
+    Settings.main();
+}
