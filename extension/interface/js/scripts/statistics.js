1e6ed910e68745f19894afb5b2537a0ae42d5eb6
Amaimersion <amatsukine@gmail.com>
2018-03-09 16:05:07 +0300

    Change a spent time rounding method

--- extension/interface/js/scripts/statistics.js
+++ extension/interface/js/scripts/statistics.js
@@ -27,7 +27,6 @@ Statistics.main = async function() {
  */
 Statistics.bindTime = function() {
     const element = document.getElementById('statistics-time');
-    console.log(SettingsIframe.userSettings.statistics);
     let time = SettingsIframe.userSettings.statistics.totalSecondsSpent / 3600;
     time = Math.floor(time);
     element.textContent = time;
