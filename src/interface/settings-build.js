babbd5cb701180dbec700a395b7622addb543506
Amaimersion <amatsukine@gmail.com>
2018-04-06 20:50:15 +0300

    Add extra css in build

--- src/interface/settings-build.js
+++ src/interface/settings-build.js
@@ -1,3 +1,4 @@
 import './js/settings.ts';
 // import './html/settings.pug';
+import './css/common/extra.scss';
 import './css/settings.scss';
--- src/interface/statistics-build.js
+++ src/interface/statistics-build.js
@@ -1,3 +1,4 @@
 import './js/statistics.ts';
 // import './html/statistics.pug';
+import './css/common/extra.scss';
 import './css/statistics.scss';
