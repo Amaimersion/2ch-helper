ef143d73ef4d626f2dbf03968e42067b68a36321
Amaimersion <amatsukine@gmail.com>
2018-04-21 21:35:34 +0300

    Add the common folder for a ts scripts with the communication module

---
+++ src/common/js/modules/Communication.ts
@@ -0,0 +1,59 @@
+export interface Message {
+    type: string;
+    name: string;
+    method?: string;
+}
+
+
+export class Page {
+    static sendMessageToContent(message: Message, responseCallback?: (response: any) => void): void {
+        responseCallback = responseCallback || function() {};
+
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
+                responseCallback(response);
+            });
+        })
+    }
+
+    static sendMessage(message: Message, responseCallback?: (response: any) => void): void {
+        responseCallback = responseCallback || function() {};
+
+        chrome.runtime.sendMessage(message, (response) => {
+            responseCallback(response);
+        });
+    }
+}
+
+
+
+//export class Content extends PageInterface {}
+
+
+//export class Background extends PageInterface {}
+
+
+/*
+Popup.sendMessageToContent = function(message: Object, callback?: (response: any) => void): void {
+    callback = callback || function() {};
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
+            callback(response);
+        });
+    });
+}
+
+
+SettingsIframe.sendMessageToContent = function(message, callback) {
+    callback = callback || function() {};
+
+    chrome.tabs.query({url: '*://2ch.hk/*//*'}, (tabs) => {
+        for (let tab of tabs) {
+            chrome.tabs.sendMessage(tab.id, message, (response) => {
+                callback(response);
+            });
+        }
+    });
+}
+*/
