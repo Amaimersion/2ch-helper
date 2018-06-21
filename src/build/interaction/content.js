4473b374d333e2b70b774756a1b4d8e302901d02
Amaimersion <amatsukine@gmail.com>
2018-06-21 17:26:36 +0300

    Add files for content build

--- src/build/interaction/content.js
+++ src/build/interaction/content.js
@@ -1,2 +1,5 @@
 import 'Interaction/content/on-message.ts';
 import 'Interaction/content/page-elements.ts';
+import 'Interaction/content/DOMContentLoaded.ts';
+import 'Interaction/content/download.ts';
+import 'Interaction/content/settings.ts';
