56c4cbf0b2df03589d41596d3ba367c565fc9850
Amaimersion <amatsukine@gmail.com>
2018-06-27 22:41:20 +0300

    Add documentation for background events.ts

--- src/interaction/background/events.ts
+++ src/interaction/background/events.ts
@@ -1,7 +1,13 @@
 import {StorageSync} from "@modules/storage-sync";
 
 
+/**
+ * Handles browser events.
+ */
 abstract class Events {
+    /**
+     * Runs when the extension is first installed.
+     */
     public static async onFirstInstall(): Promise<void> {
         await StorageSync.restoreDefault(true);
     }
@@ -13,8 +19,10 @@ chrome.runtime.onInstalled.addListener((details) => {
         Events.onFirstInstall();
     }
 
+    /*
     // only in dev mode.
     else if (details.reason === "update") {
         Events.onFirstInstall();
     }
+    */
 });
