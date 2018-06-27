3e353a266e3a4aab047868ffabb65f9ffda6c042
Amaimersion <amatsukine@gmail.com>
2018-06-27 23:38:15 +0300

    Add documentation for dom.ts

--- src/common/modules/dom.ts
+++ src/common/modules/dom.ts
@@ -1,4 +1,12 @@
+/**
+ * Handles DOMContentLoaded requests.
+ */
 export abstract class DOMLoaded {
+    /**
+     * Runs a method when the page is loaded.
+     *
+     * @param method The method for execution.
+     */
     public static runFunction(method: (...args: any[]) => any): void {
         if (document.readyState === "loading") {
             document.addEventListener("DOMContentLoaded", method);
