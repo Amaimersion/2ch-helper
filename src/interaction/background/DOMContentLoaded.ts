5d8c51e12a835300aa13cd3db4c90a120f052b67
Amaimersion <amatsukine@gmail.com>
2018-06-27 22:38:13 +0300

    Add documentation for background DOMContentLoaded.ts

--- src/interaction/background/DOMContentLoaded.ts
+++ src/interaction/background/DOMContentLoaded.ts
@@ -2,7 +2,13 @@ import {DOMLoaded} from "@modules/dom";
 import {Settings} from "./settings";
 
 
+/**
+ * Handles DOMContentLoaded event.
+ */
 abstract class DOMContentLoaded {
+    /**
+     * Runs when the page is loaded.
+     */
     public static main(): void {
         Settings.main();
     }
