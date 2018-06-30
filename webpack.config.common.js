5a7dde4ca51c1c576060578e8348e0137fb64c9b
Amaimersion <amatsukine@gmail.com>
2018-06-30 13:16:19 +0300

    Remove unnecessary imports in webpack configs

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
